01292bcf8bac9d00b8e40e90253dc89f8f51b780
olha.codes <57961694+olhacodes@users.noreply.github.com>
2024-05-12 15:51:27 +0300

    init typescript count project

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
 	<div>
-	  <p>{{ count }}</p>
-	  <button @click="reset">Reset</button>
-	</div>
+    <h1>{{ appInfo.name }}</h1>
+    <h2>{{ appInfo.version }}</h2>
+  </div>
 </template>
 
-<script>
-import useAutoCount from './composables/useAutoCount'
+<script setup lang="ts">
+import { onMounted, reactive, ref } from 'vue'; 
+import fetchCount from './utils/fetchCount';
 
-export default {
-  name: 'App',
-  setup() {
-    const { count } = useAutoCount(0)
-
-    function reset() {
-      count.value = 0
-    }
-
-    return { count, reset }
-  }
+interface AppInfo {
+  name: string;
+  version: string;
 }
+
+const count = ref<number | null>(null);
+const appInfo: AppInfo = reactive({
+  name: 'Counter',
+  version: '1.0.0',
+});
+
+onMounted(() => {
+  fetchCount(initialCount => {
+    count.value = initialCount;
+  })
+})
 </script>
